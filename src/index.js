42f0ae624414a9c5d08da218ed2ef981a5b4771c
pankaj-seecog <pankaj.7613@gmail.com>
2020-11-25 21:32:18 +0530

    2 files modified

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from "./App";
+import { createStore } from "redux";
+//named import createStore
+//store uses reducer
+//reducer contains 2 arguments 1.state of whole Store 2. action object
+//action object contains 1.type 2.payload
+let iniState = {
+  count: 0
+};
+let rootReducer = (state = iniState, action) => {
+  if (action.type == "INC") {
+    return {
+      count: state.count + 1
+    };
+  } else if (action.type == "DEC") {
+    return {
+      count: state.count - 1
+    };
+  } else if (action.type == "RESET") {
+    return {
+      count: 0
+    };
+  }
+  return state;
+};
+
+let store = createStore(rootReducer);
+console.log("The store is ", store);
 
 ReactDOM.render(<App />, document.getElementById("root"));
